68a0e89765cb54d2febe442ee8105d2d9be76cc7
kamlapatiportfolio <kamlapatiportfolio@gmail.com>
2026-05-07 12:45:01 +0530

    fix: remove Node version conflict in vercel.json, add env, fix chunk size

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          firebase: ['firebase/app', 'firebase/auth', 'firebase/storage', 'firebase/firestore'],
+        }
+      }
+    }
+  }
 })
